4c36189543bb08fce0a66130139d07fb86eea696
root <root@c55ef46ffb76>
2020-04-14 08:52:08 +0000

    fix

--- Server/update.bash
+++ Server/update.bash
@@ -1,6 +1,6 @@
 while [[ 1 ]]; do
   data=$(docker stats --format "{{json .}}," --no-stream -a);
   mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-  proc=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+  proc=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
   echo "{\"memory\" : $mem, \"CPU\": $proc, \"containers\":  [${data::-1}]}" > /var/www/html/index.html;
 done;
