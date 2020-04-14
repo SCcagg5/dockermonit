35563ae6576ee36c0efa49d1e1242e0b980b9781
root <root@3c6514bb27ef>
2020-04-14 08:28:12 +0000

    fixing index

--- Server/update.bash
+++ Server/update.bash
@@ -2,5 +2,5 @@ while [[ 1 ]]; do
   data=$(docker stats --format "{{json .}}," --no-stream -a);
   mem=$(echo "scale=2; $(free -m | grep -oP '\d+' | head -n 2 | tail -n 1) / $(free -m | grep -oP '\d+' | head -n 6 | tail -n 1) * 100" | bc -l)
   proc=$(echo "scale=2; 100 - $(iostat -c | tail -n 3 | head -n 1 | cut -d' ' -f31)" | bc -l)
-  echo "{\"memory\" : $mem, \"CPU\": $proc, \"containers\":  [${data::-1}]}" > index.html
+  echo "{\"memory\" : $mem, \"CPU\": $proc, \"containers\":  [${data::-1}]}" > /var/www/html/index.html;
 done;
