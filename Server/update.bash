6579a80a363221d282ad8f11f113597559513613
root <root@3c6514bb27ef>
2020-04-14 08:37:21 +0000

    fixing meme

--- Server/update.bash
+++ Server/update.bash
@@ -1,6 +1,6 @@
 while [[ 1 ]]; do
   data=$(docker stats --format "{{json .}}," --no-stream -a);
-  mem=$(echo "scale=2; $(free -m | grep -oP '\d+' | head -n 2 | tail -n 1) / $(free -m | grep -oP '\d+' | head -n 6 | tail -n 1) * 100" | bc -l)
+  mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
   proc=$(echo "scale=2; 100 - $(iostat -c | tail -n 3 | head -n 1 | cut -d' ' -f31)" | bc -l)
   echo "{\"memory\" : $mem, \"CPU\": $proc, \"containers\":  [${data::-1}]}" > /var/www/html/index.html;
 done;
