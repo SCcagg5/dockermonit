a646cb8143135608cb0ebcc44fcc1e8fe295dd07
root <root@c55ef46ffb76>
2020-04-14 08:48:01 +0000

    proc usage

--- Server/update.bash
+++ Server/update.bash
@@ -1,6 +1,6 @@
 while [[ 1 ]]; do
   data=$(docker stats --format "{{json .}}," --no-stream -a);
   mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-  proc=$(echo "scale=2; 100 - $(iostat -c | tail -n 3 | head -n 1 | cut -d' ' -f31)" | bc -l)
+  proc=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
   echo "{\"memory\" : $mem, \"CPU\": $proc, \"containers\":  [${data::-1}]}" > /var/www/html/index.html;
 done;
